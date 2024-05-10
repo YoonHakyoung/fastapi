bd36925a3f56d7e1087359cc6fa2c713efd4da46
윤하경 <yhkyhk0816@gmail.com>
2024-05-10 16:16:20 +0900

    get method 틀

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ app = FastAPI()
 # 테스트 삭제
 
 # 테스트 케이스 목록 가져오기
+@app.get("/testcase/{testcase_id}")
+def read_list():
+    # 테스트 케이스 가져오는 SQL
+    return 
 
 # runner 실행
 
 # 테스트 결과값 반환
+@app.get("/testcase/{testcase_id}/stats")
+def stats():
+    # 테스트 결과 가져오는 SQL
+    return
